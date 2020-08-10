1655eb4646a74353b593ded93fb82e90a80d8594
ari-999 <57689938+ari-999@users.noreply.github.com>
2020-08-10 13:03:22 -0400

    Add files via upload

---
+++ ThisMod/Items/Weapons/Meelee/DragonFlail/DragonFlail.cs
@@ -0,0 +1,39 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ThisMod.Items.Weapons.Meelee.DragonFlail
+{
+	public class DragonFlail : ModItem
+	{
+		public override void SetDefaults()
+		{
+			item.width = 22;
+			item.height = 20;
+			item.value = Item.sellPrice(silver: 5);
+			item.rare = ItemRarityID.White;
+			item.noMelee = true;
+			item.useStyle = ItemUseStyleID.HoldingOut;
+			item.useAnimation = 40;
+			item.useTime = 40;
+			item.knockBack = 4f;
+			item.damage = 9;
+			item.noUseGraphic = true;
+			item.shoot = ModContent.ProjectileType<DragonFlailProjectile>();
+			item.shootSpeed = 15.1f;
+			item.UseSound = SoundID.Item1;
+			item.melee = true;
+			item.crit = 9;
+			item.channel = true;
+		}
+
+		public override void AddRecipes()
+		{
+			var recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Chain);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
